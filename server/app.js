d37d4f955155195eb1bf9282146a798279c01e67
mculotta120 <margaret.culotta@gmail.com>
2016-06-08 10:55:07 -0500

    hello world

--- server/app.js
+++ server/app.js
@@ -3,3 +3,9 @@ var app = express();
 var server = app.listen(process.env.PORT || 3000, function(){
   console.log('server listening');
 });
+
+app.get('/', function(req,res){
+  res.writeHead(200);
+  res.write('hello world');
+  res.end();
+});
